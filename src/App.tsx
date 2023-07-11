41d9540f2357e2b75ccb9b91c306a1a35bf03d12
Yuki Kobayashi <yuki.kobayashi@japan-systems.co.jp>
2023-07-11 22:37:12 +0900

    課題1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import type { ChangeEvent } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [list, setList] = useState<String[]>([]);
+
+  const handleGetText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleGetList = () => {
+    setList([...list, text]);
+    setText("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={text} onChange={handleGetText} />
+      <button onClick={handleGetList}>追加</button>
+      <ul>
+        {list.map((text, index) => (
+          <li key={index}>{text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
